0a1db69d995884e719ee553394fc7ddaa1976493
Niall O'Hara <niall@changex.org>
2019-05-20 17:48:26 +0100

    added app html template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def template_test():
+    return render_template('template.html', label='', imagesource='../uploads/template.jpg')
 
 
 @app.route('/<name>')
